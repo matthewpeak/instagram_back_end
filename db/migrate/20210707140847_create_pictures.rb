4d8400377c034d0b5478bdf8e371e81ffd93d6f4
matthewpeak <matthewpeak@gmail.com>
2021-07-13 12:41:29 -0400

    edited tables

--- db/migrate/20210707140847_create_pictures.rb
+++ db/migrate/20210707140847_create_pictures.rb
@@ -3,6 +3,9 @@ class CreatePictures < ActiveRecord::Migration[6.0]
     create_table :pictures do |t|
       t.string :title
       t.integer :likes
+      t.boolean :liked
+      t.integer :commentCount
+      t.string :address
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_161410) do
+ActiveRecord::Schema.define(version: 2021_07_07_140847) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.integer "likes"
+    t.boolean "liked"
+    t.integer "commentCount"
+    t.string "address"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -20,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_06_30_161410) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pictures", "users"
 end
